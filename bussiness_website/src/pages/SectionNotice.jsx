b08e510db0e4b983fee5e007f5f8cadebdb26392
eman <manoxhk2710@gmail.com>
2025-01-16 11:45:11 +0500

    section page change text

--- bussiness_website/src/pages/SectionNotice.jsx
+++ bussiness_website/src/pages/SectionNotice.jsx
@@ -80,7 +80,7 @@ const SectionNotice = () => {
         <div className="py-10 text-center">
           <button
             type="button"
-            class="py-2.5 px-5 me-2 mb-2 text-sm font-medium focus:outline-none bg-Green text-white rounded-full  hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+            class="py-2.5 px-5 me-2 mb-2 text-sm font-medium focus:outline-none bg-Green text-white rounded-full  hover:bg-gray-100 hover:text-black focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
           >
             0 800 118 4 118
           </button>
